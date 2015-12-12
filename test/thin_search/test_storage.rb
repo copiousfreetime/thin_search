899b51853ea135089eeb0ce08a3953de6e16c3cc
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-11 21:01:02 -0700

    use store accessor

--- test/thin_search/test_storage.rb
+++ test/thin_search/test_storage.rb
@@ -3,14 +3,14 @@ require 'thin_search/store'
 
 class TestStorage < ::ThinSearch::Test
   def test_creates_storage_index
-    @store.create_index("test_storage")
-    assert(@store.has_index?("test_storage"))
+    store.create_index("test_storage")
+    assert(store.has_index?("test_storage"))
   end
 
   def test_deletes_storage_index
-    @store.create_index("test_storage")
-    assert(@store.has_index?("test_storage"))
-    @store.drop_index("test_storage")
-    refute(@store.has_index?("test_storage"), "test_storage exists")
+    store.create_index("test_storage")
+    assert(store.has_index?("test_storage"))
+    store.drop_index("test_storage")
+    refute(store.has_index?("test_storage"), "test_storage exists")
   end
 end
