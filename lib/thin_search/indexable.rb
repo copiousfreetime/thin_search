1491f1f07874676d821453256d8b20d51bf6c004
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-14 15:00:44 -0700

    test Indexable hooks things up

--- lib/thin_search/indexable.rb
+++ lib/thin_search/indexable.rb
@@ -3,12 +3,29 @@ require 'thin_search/document'
 module ThinSearch
   # Public: This is the manner in which an object is indexed.
   #
-  # An object that wants to be indexed includs this module in its class
+  # An object that wants to be indexed includs this module in its class and then
+  # invokes the class methdo :indexable
+  #
+  # Example:
+  #
+  #   class IndexableModel
+  #     include ::ThinSearch::Indexable
+  #
+  #     indexable :context_id => lambda { |i| i.id },
+  #               :finder     => lambda { |id| ::IndexableModel.find_by(:id => id) },
+  #               :facets     => lambda { |i| { :date => i.date, :department => i.department, :color => i.color } },
+  #               :important  => lambda { |i| [ i.email, i.name ] },
+  #               :normal     => :bio
+  #   end
+  #
   module Indexable
 
     module ClassMethods
       def indexable( opts = {} )
-        ThinSearch::Conversion.register(self, opts) # self registers
+        unless opts.has_key?(:context) || opts.has_key?('context') then
+          opts[:context] = self
+        end
+        ThinSearch::Conversion.register(opts) # self registers
       end
     end
 
