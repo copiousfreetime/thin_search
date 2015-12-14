5ed4eee8d69f13d6c26545b8737d1b8e1ec46f0e
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-13 23:01:57 -0700

    test removing multiple docs

--- test/thin_search/test_index.rb
+++ test/thin_search/test_index.rb
@@ -48,4 +48,17 @@ class TestIndex < ::ThinSearch::Test
     count = index.count
     assert_equal(docs.size - 1, count)
   end
+
+  def test_can_remove_multiple_documents
+    docs = Array.new(10) { fake_document }
+    index.add(docs)
+    count = store.document_count_for(index_name)
+    assert_equal(docs.count, count)
+
+    removing = docs.shift(5)
+    index.remove(removing)
+    count = store.document_count_for(index_name)
+    assert_equal(docs.count, count)
+  end
+
 end
