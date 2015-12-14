daad83821aa855ccee47ed46da364fcb31e14695
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-13 21:42:36 -0700

    cache instance of operation classes for each index

--- lib/thin_search/store.rb
+++ lib/thin_search/store.rb
@@ -8,15 +8,16 @@ module ThinSearch
     attr_reader :db
 
     def initialize(path)
-      @db = ::Amalgalite::Database.new(path.to_s)
+      @db               = ::Amalgalite::Database.new(path.to_s)
+      @index_operations = Hash.new
     end
 
     def create_index(index_name)
-      StoreOperations::CreateIndex.new(index_name).call(db)
+      operations_for_index(index_name)[StoreOperations::CreateIndex].call(db)
     end
 
     def drop_index(index_name)
-      StoreOperations::DropIndex.new(index_name).call(db)
+      operations_for_index(index_name)[StoreOperations::DropIndex].call(db)
     end
 
     def has_index?(name)
@@ -24,50 +25,35 @@ module ThinSearch
     end
 
     def add_document_to_index(index_name, document)
-      operation = StoreOperations::Insert.new(index_name)
-      operation.call(db, document)
+      operations_for_index(index_name)[StoreOperations::Insert].call(db, document)
     end
 
     def add_documents_to_index(index_name, documents)
-      operation = StoreOperations::BulkInsert.new(index_name)
-      operation.call(db, documents)
+      operations_for_index(index_name)[StoreOperations::BulkInsert].call(db, documents)
     end
 
     def document_count_for(index_name)
-      operation = StoreOperations::DocumentCount.new(index_name)
-      operation.call(db)
+      operations_for_index(index_name)[StoreOperations::DocumentCount].call(db)
     end
 
     def search_index(index_name, query, &block)
-      operation = StoreOperations::Search.new(index_name)
-      operation.call(db, query) do |document|
+      search_op = operations_for_index(index_name)[StoreOperations::Search]
+      search_op.call(db, query) do |document|
         yield document
       end
     end
 
-    def document_to_select_rowid_bindings(doc)
-      {
-        ':query'      => "'\"context:#{doc.context} AND context_id:#{doc.context_id}\"'",
-        ':context'    => doc.context,
-        ':context_id' => doc.context_id,
+    private
+
+    def operations_for_index(index_name)
+      @index_operations[index_name] || {
+        StoreOperations::CreateIndex   => StoreOperations::CreateIndex.new(index_name),
+        StoreOperations::DropIndex     => StoreOperations::DropIndex.new(index_name),
+        StoreOperations::Insert        => StoreOperations::Insert.new(index_name),
+        StoreOperations::BulkInsert    => StoreOperations::BulkInsert.new(index_name),
+        StoreOperations::DocumentCount => StoreOperations::DocumentCount.new(index_name),
+        StoreOperations::Search        => StoreOperations::Search.new(index_name),
       }
-    end
-
-    def select_rowid_sql(index_name)
-      @sql_cache["#{index_name}.select_rowid"] || <<-SQL
-      SELECT rowid, *
-        FROM #{index_name}
-       WHERE #{index_name} MATCH :query
-         AND context = :context
-         AND context_id = :context_id
-      SQL
-    end
-
-    def delete_sql(index_name)
-      @sql_cache["#{index_name}.delete"] ||<<-SQL
-      DELETE FROM #{index_name}
-       WHERE rowid = ?
-      SQL
     end
   end
 end
