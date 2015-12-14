ee2122b25757fd0c1be754fdd9f3efeff91967b6
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-14 13:42:20 -0700

    Index objects not documents and update tests accordingly

--- lib/thin_search/index.rb
+++ lib/thin_search/index.rb
@@ -1,5 +1,6 @@
 require 'amalgalite'
 require 'thin_search/store'
+require 'thin_search/conversion'
 
 module ThinSearch
   class Index
@@ -15,20 +16,26 @@ module ThinSearch
       @store.create_index(@name) unless @store.has_index?(@name)
     end
 
+    # Public: Return the number of documents in the index
+    #
+    # Returns integer
     def count
       @store.document_count_for(name)
     end
 
+    # Public: Add one or more documents to the index
+    #
+    # indexable - an indexable document, or an array of indexable documents
+    #
+    # Returns nothing
     def add(indexable)
-      Array(indexable).tap do |list|
-        store.add_documents_to_index(name, list)
-      end
+      documents = to_indexable_documents(indexable)
+      store.add_documents_to_index(name, documents)
     end
 
     def remove(indexable)
-      Array(indexable).tap do |list|
-        store.remove_documents_from_index(name, list)
-      end
+      documents = to_indexable_documents(indexable)
+      store.remove_documents_from_index(name, documents)
     end
 
     # Public: Find a particular document in the index
@@ -38,7 +45,8 @@ module ThinSearch
     #
     # Returns a Document or nil if not found
     def find(indexable)
-      store.find_one_document_in_index(name, indexable)
+      document = Conversion.to_indexable_document(indexable)
+      store.find_one_document_in_index(name, document)
     end
 
     # Public: Update a single document in the index
@@ -49,8 +57,26 @@ module ThinSearch
     #
     # Returns a Document or nil
     def update(indexable)
-      store.update_document_in_index(name, indexable)
+      document = Conversion.to_indexable_document(indexable)
+      store.update_document_in_index(name, document)
     end
 
+    # Public: Search for and yield the resulting items
+    #
+    # yields each orginal class instance
+    #
+    # Returns nothing
+    def search(query, &block)
+      store.search_index(name, query) do |document|
+        yield Conversion.from_indexable_document(document)
+      end
+    end
+
+    private
+
+    def to_indexable_documents(indexable)
+      list = Array(indexable)
+      list.map { |item| Conversion.to_indexable_document(item) }
+    end
   end
 end
--- test/thin_search/test_index.rb
+++ test/thin_search/test_index.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
+require 'test_model'
 require 'thin_search/store'
 require 'thin_search/index'
+require 'thin_search/conversion'
 
 class TestIndex < ::ThinSearch::Test
 
@@ -10,10 +12,27 @@ class TestIndex < ::ThinSearch::Test
 
   def setup
     super
+    ::ThinSearch::Conversion.register({
+      :context    => "TestModel",
+      :context_id => :id,
+      :finder     => :find_by_id,
+      :facets     => :thinsearch_facets,
+      :important  => :thinsearch_important,
+      :normal     => :thinsearch_normal,
+    }
+    )
+
+    TestModel.populate(10)
+    @docs       = TestModel.collection.values
     @index_name = "test_index"
     @index      = ::ThinSearch::Index.new(:store => store, :name => @index_name)
-    @docs       = Array.new(10) { fake_document }
     @index.add(@docs)
+  end
+
+  def teardown
+    super
+    TestModel.clear
+    ThinSearch::Conversion.registry.clear
   end
 
   def test_creates_index_on_instantiation
@@ -22,7 +41,7 @@ class TestIndex < ::ThinSearch::Test
 
   def test_can_index_a_document
     before_count = store.document_count_for(index_name)
-    index.add(fake_document)
+    index.add(TestModel.new)
     after_count = store.document_count_for(index_name)
 
     assert_equal(1, after_count - before_count)
@@ -62,18 +81,30 @@ class TestIndex < ::ThinSearch::Test
     find_me = docs.last
     doc = index.find(find_me)
 
-    assert_equal(find_me.context, doc.context)
-    assert_equal(find_me.context_id, doc.context_id)
+    assert_equal(find_me.class.to_s, doc.context)
+    assert_equal(find_me.id, doc.context_id)
   end
 
   def test_can_update_a_document
     find_me = docs.last
-    normal  = "testupdatesdocument"
-    find_me.normal = normal
+    dept    = "testupdatesdocument"
+    find_me.department = dept
 
     index.update(find_me)
     doc = index.find(find_me)
-    assert_equal(find_me.context, doc.context)
-    assert_equal(find_me.context_id, doc.context_id)
+    assert_equal(find_me.class.to_s, doc.context)
+    assert_equal(find_me.id, doc.context_id)
+  end
+
+  def test_yields_each_found_document
+    expected = docs.select { |doc| doc.email =~ /gmail/ }
+
+    found = []
+    index.search("gmail") do |doc|
+      found << doc
+    end
+
+    assert(expected.size, found.size)
+
   end
 end
