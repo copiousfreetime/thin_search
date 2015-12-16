f2afc9af83d27bf2c49a266a0ccaa2ce8898404a
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-16 16:27:36 -0700

    Batch retrieve documents from model sources

--- lib/thin_search/query_result.rb
+++ lib/thin_search/query_result.rb
@@ -7,11 +7,15 @@ module ThinSearch
     attr_reader :total_count
 
     def initialize(query, documents, total_count = nil)
-      @query         = query
-      @raw_documents = documents
-      @total_count   = total_count || @row_documents.size
-      @models        = []
-      @missing       = []
+      @query               = query
+      @raw_documents       = documents
+      @total_count         = total_count || @raw_documents.size
+      @models              = []
+      @missing             = []
+      @ordered_index_ids   = []
+      @grouped_context_ids = Hash.new { |h,k| h[k] = Array.new }
+      @models_by_index_id  = Hash.new
+      prep_for_inflation
     end
 
     # Public: Return the original objects that were indexed. 
@@ -22,7 +26,7 @@ module ThinSearch
     #
     # Returns Array of models
     def models
-      inflate_models if @models.empty?
+      inflate_and_order_models if @models.empty?
       @models
     end
 
@@ -40,12 +44,36 @@ module ThinSearch
 
     private
 
-    def inflate_models
+    def prep_for_inflation
       raw_documents.each do |document|
-        begin
-          @models << ::ThinSearch::Conversion.from_indexable_document(document)
-        rescue Object
-          @missing << document
+        @ordered_index_ids << document.index_unique_id
+        @grouped_context_ids[document.context] << document.context_id
+      end
+    end
+
+    def inflate_and_order_models
+      inflate_models
+      order_models
+    end
+
+    def inflate_models
+      @grouped_context_ids.each do |context, context_ids|
+        context_models = inflate_context(context, context_ids)
+        context_models.each do |model|
+          @models_by_index_id[model._thin_search_index_unique_id] = model
+        end
+      end
+    end
+
+    def inflate_context(context, context_ids)
+      conversion = ::ThinSearch::Conversion.for(context)
+      conversion.batch_find_by_ids(context_ids)
+    end
+
+    def order_models
+      @ordered_index_ids.each do |index_unique_id|
+        if model = @models_by_index_id[index_unique_id] then
+          @models << model
         end
       end
     end
---
+++ test/thin_search/test_query_result.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+require 'test_model'
+require 'thin_search/index'
+require 'thin_search/indexable'
+require 'thin_search/query'
+require 'thin_search/query_result'
+
+
+class TestQueryResult < ::ThinSearch::Test
+
+  class TestModel2 < TestModel
+    Collection = Hash.new
+    include ::ThinSearch::Indexable
+
+    indexable :context_id => :id,
+      :finder       => lambda { |i| TestModel2::Collection[i] },
+      :batch_finder => lambda { |ids| ids.map { |i| TestModel2::Collection[i] } },
+      :facets       => :thinsearch_facets,
+      :important    => :thinsearch_important,
+      :normal       => :thinsearch_normal
+  end
+
+  class TestModel3 < TestModel
+    Collection = Hash.new
+    include ::ThinSearch::Indexable
+
+    indexable :context_id => :id,
+      :finder       => lambda { |i| TestModel3::Collection[i] },
+      :batch_finder => lambda { |ids| ids.map { |i| TestModel3::Collection[i] } },
+      :facets       => :thinsearch_facets,
+      :important    => :thinsearch_important,
+      :normal       => :thinsearch_normal
+  end
+
+
+  #* have an index option to return the Documents instead of the objects
+  #* efficient inflation of large numbers of objects from Documents
+
+  #* REsults needs to have .total_pages .num_pages .current_page .
+  #* show documents, missing documents in original via _thinsearch_documents
+  # * and use .models to inflate at the end.
+
+  attr_reader :collection_1
+  attr_reader :collection_2
+
+  def setup
+    super
+    TestModel2::Collection.clear
+    TestModel2::Collection.merge!(TestModel2.generate_collection(10))
+
+    TestModel3::Collection.clear
+    TestModel3::Collection.merge!(TestModel3.generate_collection(10))
+
+    @index_name = "test_query_result_index"
+    @index      = ::ThinSearch::Index.new(:store => store, :name => @index_name)
+    @index.add(TestModel2::Collection.values)
+    @index.add(TestModel3::Collection.values)
+    @query = @index.search("gmail")
+  end
+
+  def test_inflates_to_models
+    result = @query.execute
+    models = result.models
+    models.each do |m|
+      assert([::TestQueryResult::TestModel2, ::TestQueryResult::TestModel3].include?(m.class))
+    end
+  end
+
+end
+
