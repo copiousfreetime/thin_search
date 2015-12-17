187af5eb0c1fdc774945025f35a01c8bbeb8221d
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-12-16 20:47:03 -0700

    make it more functional

--- lib/thin_search/query_result.rb
+++ lib/thin_search/query_result.rb
@@ -1,33 +1,27 @@
+require 'forwardable'
 module ThinSearch
   class QueryResult
 
+    include Enumerable
+
     attr_reader :raw_documents
     attr_reader :query
-    attr_reader :missing
     attr_reader :total_count
+    attr_reader :models
+
+    extend Forwardable
+    def_delegator :@models, :each
+
 
     def initialize(query, documents, total_count = nil)
       @query               = query
       @raw_documents       = documents
       @total_count         = total_count || @raw_documents.size
-      @models              = []
-      @missing             = []
-      @ordered_index_ids   = []
-      @grouped_context_ids = Hash.new { |h,k| h[k] = Array.new }
-      @models_by_index_id  = Hash.new
-      prep_for_inflation
+      @models              = inflate_documents_to_models(raw_documents)
     end
 
-    # Public: Return the original objects that were indexed. 
-    #
-    # This will convert the search documents back to their original Model
-    # objects. If there are problems converting some of the documents back, then
-    # those original raw documents will be stored in #missing
-    #
-    # Returns Array of models
-    def models
-      inflate_and_order_models if @models.empty?
-      @models
+    def each(&block)
+      models.each(&block)
     end
 
     def size
@@ -44,23 +38,31 @@ module ThinSearch
 
     private
 
-    def prep_for_inflation
-      raw_documents.each do |document|
-        @ordered_index_ids << document.index_unique_id
-        @grouped_context_ids[document.context] << document.context_id
+    def inflate_documents_to_models(documents)
+
+     # Track the documents original order
+      ordered_index_ids   = []
+      grouped_context_ids = Hash.new { |h,k| h[k] = Array.new }
+
+      documents.each do |document|
+        ordered_index_ids << document.index_unique_id
+        grouped_context_ids[document.context] << document.context_id
       end
+
+     # Batch inflate the grouped ids efficiently
+      models_by_index_id = inflate_models(grouped_context_ids)
+
+      # Order the inflated models by the original order
+      order_models(ordered_index_ids, models_by_index_id)
     end
 
-    def inflate_and_order_models
-      inflate_models
-      order_models
-    end
-
-    def inflate_models
-      @grouped_context_ids.each do |context, context_ids|
-        context_models = inflate_context(context, context_ids)
-        context_models.each do |model|
-          @models_by_index_id[model._thin_search_index_unique_id] = model
+    def inflate_models(grouped_context_ids)
+      Hash.new.tap do |models_by_index_id|
+        grouped_context_ids.each do |context, context_ids|
+          context_models = inflate_context(context, context_ids)
+          context_models.each do |model|
+            models_by_index_id[model._thin_search_index_unique_id] = model
+          end
         end
       end
     end
@@ -70,12 +72,15 @@ module ThinSearch
       conversion.batch_find_by_ids(context_ids)
     end
 
-    def order_models
-      @ordered_index_ids.each do |index_unique_id|
-        if model = @models_by_index_id[index_unique_id] then
-          @models << model
+    def order_models(ordered_index_ids, models_by_index_id)
+      Array.new.tap do |models|
+        ordered_index_ids.each do |index_unique_id|
+          if model = models_by_index_id[index_unique_id] then
+            models << model
+          end
         end
       end
     end
+
   end
 end
